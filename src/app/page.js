f0ccf40cbb38a33880b72785d52700f0a4ff8078
pixelupmrk <pixelupmrkt@gmail.com>
2026-01-08 21:48:51 -0300

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>🚗 SuperApp Cars - Online</h1>
+      <p>O sistema foi conectado ao banco de dados com sucesso.</p>
+    </div>
+  );
+}
