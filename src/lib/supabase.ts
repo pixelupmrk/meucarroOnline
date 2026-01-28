3c40bb5574dd3e2670d025f1d75e99084706cc85
PixelUP <pixelupmrkt@gmail.com>
2026-01-28 17:47:30 -0300

    fix: injetando arquivo lib/supabase que faltava para build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,4 @@
 ﻿import { createClient } from '@supabase/supabase-js'
-
-// O código busca estes nomes exatos em inglês
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
